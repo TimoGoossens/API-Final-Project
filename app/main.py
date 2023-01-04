b3fadadc9987b791e588d603b5b924a532528327
TimoGoossens <r0891940@student.thomasmore.be>
2023-01-04 15:00:48 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -67,6 +67,11 @@ def read_user(player_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Player not found")
     return db_player
 
+@app.put("/update/player/{player_id}", response_model=schemas.Player)
+async def update_player(player: schemas.PlayerCreate, db: Session = Depends(get_db),player_id: int = Path(ge=0, le=60, default=1)):
+    return crud.update_player(db=db, player=player, player_id=player_id)
+    
+
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
