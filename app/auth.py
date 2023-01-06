a5888f6a58252aa91de32777351df8814b53069d
TimoGoossens <r0891940@student.thomasmore.be>
2023-01-06 10:30:11 +0100

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -25,7 +25,7 @@ def authenticate_user(db: Session, username: str, password: str):
     user = crud.get_user_by_email(db, username)
     if not user:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(password, username.hashed_password):
         return False
     return user
 
