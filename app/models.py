0a05e10fc98c5ebbf8a8379915799c3d043a0cff
TimoGoossens <r0891940@student.thomasmore.be>
2023-01-06 11:46:31 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -17,8 +17,7 @@ class Player(Base):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True) 
+    id = Column(Integer, primary_key=True, index=True) 
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
