3aee7cd92784ff06b0bf5ed483851949b2b011b9
TimoGoossens <r0891940@student.thomasmore.be>
2023-01-06 10:33:50 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -18,9 +18,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True) 
+    username = Column(String, unique=True, index=True) 
     email = Column(String, unique=True, index=True)
-    secure_password = Column(String)
+    hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
     items = relationship("Item", back_populates="owner")
